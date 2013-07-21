b1a25c42d8bb93e6416bca9385f2672bfbe32962
Denis Nagibin <denis.nagibin@gmail.com>
2013-07-21 11:44:11 +0400

    Task E has been completed

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -54,9 +54,11 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
+    @cart = current_cart
     @cart.destroy
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url }
+      format.html { redirect_to store_url, notice: 'Your cart is empty now!' }
       format.json { head :no_content }
     end
   end
@@ -64,7 +66,12 @@ class CartsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
-      @cart = Cart.find(params[:id])
+      begin
+        @cart = Cart.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        logger.error "Wrong access to #{params[:id]}"
+        redirect_to store_url, notice: 'Nonexistent cart'
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
